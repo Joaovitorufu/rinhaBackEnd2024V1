4647bb6a2518dba4acf6365b28673fe047fc24bc
Joaovitorufu <joaovitorufu@gmail.com>
2024-06-07 14:43:38 -0300

    first commit

---
+++ src/main/java/com/rinha2024v1/desafio/DesafioApplication.java
@@ -0,0 +1,14 @@
+package com.rinha2024v1.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class DesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rinha2024v1/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinha2024v1.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
